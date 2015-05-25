886e7bacd7290009957c1b82eca09fc15a2c98e3
Eric Buckley <andrewlandry@gmail.com>
2015-05-25 19:40:09 -0400

    create notices model

--- app/Http/Controllers/NoticesController.php
+++ app/Http/Controllers/NoticesController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 
 use App\Http\Requests\PrepareNoticeRequest;
 use App\Provider;
+use Illuminate\Auth\Guard;
 use Illuminate\Http\Request;
 
 class NoticesController extends Controller {
@@ -28,9 +29,47 @@ class NoticesController extends Controller {
 		return view('notices.create', compact('providers'));
 	}
 
-	public function confirm(PrepareNoticeRequest $request)
+	/**
+	 * Ask the user to confirm the DMCA that will be delivered
+	 *
+	 * @param PrepareNoticeRequest $request
+	 * @param Guard $auth
+	 * @return \Illuminate\View\View
+	 */
+	public function confirm(PrepareNoticeRequest $request, Guard $auth)
 	{
-		return $request->all();
+		$template = $this->compileDmcaTemplate($data = $request, $auth);
+
+		session()->flash('dmca', $data->all());
+
+		return view('notices.confirm', compact('template'));
+	}
+
+	public function store()
+	{
+		// Form data is flashed. Get with session()->get('dmca')
+		// Template is in request. Request::input('template')
+		// So build up a Notice object (create table too)
+		// persist it with this data
+		// And then fire off the email
+	}
+
+	/**
+	 * Compile the DMCA template from the form data
+	 *
+	 * @param PrepareNoticeRequest $data
+	 * @param Guard $auth
+	 * @return mixed
+	 */
+	private function compileDmcaTemplate(PrepareNoticeRequest $data, Guard $auth)
+	{
+		$data = $data->all() + [
+				'name' => $auth->user()->name,
+				'email' => $auth->user()->email
+			];
+
+		$template = view()->file(app_path('Http/Templates/dmca.blade.php'), $data);
+		return $template;
 	}
 
 }
